f96a1ca50a02227576ad0452189f468768c5a6e3
j-bresson <jean.bresson.mail@gmail.com>
2022-02-20 14:31:29 +0100

    Update Lisp tests

--- Lisp/omaudiolib-test.lisp
+++ Lisp/omaudiolib-test.lisp
@@ -20,56 +20,80 @@
 ;;; load the bindings
 (load (merge-pathnames "omaudiolib.lisp" *load-pathname*))
 
-;;; set a test player
-(defvar +test-juce-player+)
-(setf +test-juce-player+ (juce::openAudioManager))
+
+(defun test-print (format-string &optional args)
+  (apply 'format (append (list t (concatenate 'string "~%:omaudiolib-test: "
+                                              (substitute #\~ #\$ format-string :test 'string-equal)))
+                         args)))
+
+
+(test-print (juce::versionstring))
 
 ;;; get info about available devices
-(juce::getDevicesTypeCount +test-juce-player+)
-(juce::getDeviceTypeName +test-juce-player+ 0)
-(juce::getInputDevicesCountForType +test-juce-player+ 0)
-(juce::getOutputDevicesCountForType +test-juce-player+ 0)
+
+(defun scan-devices ()
+  (let* ((player (juce::openAudioManager))
+         (n-types (juce::getDevicesTypeCount player)))
+    (test-print "Number of devices types: $A" (list n-types))
+    (dotimes (type n-types)
+      (test-print "Device type $A: $A"(list type (juce::getDeviceTypeName player type)))
+      (let ((n-out-devices (juce::getOutputDevicesCountForType player type))
+            (n-in-devices (juce::getInputDevicesCountForType player type)))
+        (test-print "  Number of output devices: $A" (list n-out-devices))
+        (dotimes (out n-out-devices)
+          (test-print "  - $A" (list (juce::getNthOutputDeviceName player type out))))
+        (test-print "  Number of input devices: $A" (list n-in-devices))
+        (dotimes (in n-in-devices)
+          (test-print "  - $A" (list (juce::getNthInputDeviceName player type in))))
+        ))
+    (juce::closeAudioManager player)))
+
+; (scan-devices)
 
 
-(let ((n-types (juce::getDevicesTypeCount +test-juce-player+)))
-  (loop for type from 0 to (1- n-types) append
-        (let ((type-name (juce::getDeviceTypeName +test-juce-player+ type)))
-          (loop for n from 0 to (1- (juce::getOutputDevicesCountForType +test-juce-player+ type))
-                collect (list type-name (juce::getNthOutputDeviceName +test-juce-player+ type n)))
-          )))
+(defun print-state (player)
+  (test-print "Current device: $A" (list (juce::getCurrentDeviceName player)))
+  (test-print "  Sample rates: $A" (list (juce::getsamplerates player)))
+  (test-print "  - Current sample rate: $A" (list (juce::getcurrentsamplerate player)))
+  (test-print "  Buffer sizes: $A" (list (juce::getbuffersizes player)))
+  (test-print "  - Current buffer size: $A"(list (juce::getcurrentbuffersize player))))
 
-(let ((n-types (juce::getDevicesTypeCount +test-juce-player+)))
-	  (loop for type from 0 to (1- n-types) append
-	       (let ((type-name (juce::getDeviceTypeName +test-juce-player+ type)))
-		 (loop for n from 0 to (1- (juce::getInputDevicesCountForType +test-juce-player+ type))
-		    collect (list type-name (juce::getNthInputDeviceName +test-juce-player+ type n)))
-		 )))
+(defparameter *DEVICE* 0)
 
+(defun init-audio-device (n)
+  (let ((player (juce::openAudioManager)))
+    (assert (< n (juce::getDevicesTypeCount player)))
+    (test-print "Setting device type: $A" (list (juce::getDeviceTypeName player n)))
+    (juce::setdevicetype player (juce::getDeviceTypeName player n))
+    (juce::setOutputDevice player 0)
+    (print-state player)
+    player))
 
-;;; setup a player
-;; (juce::setdevicetype +test-juce-player+ "JACK")
-;; (juce::setdevicetype +test-juce-player+ "ALSA")
+; (progn (let ((p (init-audio-device 0))) (juce::closeAudioManager p)))
 
-(juce::audio-driver-output-devices
- +test-juce-player+
- (juce::getCurrentDeviceType +test-juce-player+))
-
-
-(juce::getcurrentsamplerate +test-juce-player+)
-(juce::getbuffersizes +test-juce-player+)
-(juce::getavailablesampleratescount +test-juce-player+)
-(juce::getsamplerates +test-juce-player+)
 
 ;; test play
+
+(defun play (device soundfile)
+  (let ((player (init-audio-device device))
+        (source (juce::makeaudiosourcefromfile soundfile)))
+   (juce::startAudioSource player source)
+   (sleep 1)
+   (juce::pauseAudioSource player source)
+   (sleep 1)
+   (juce::startAudioSource player source)
+   (sleep 1)
+   (juce::stopAudioSource player source)
+   (sleep 1)
+   (juce::startAudioSource player source)
+   (sleep 1)
+   (juce::stopAudioSource player source)
+
+   (juce::freeaudiosource source)
+   (juce::closeAudioManager player)))
+
 ;; (defparameter *soundfile* "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/Bassclarinet2.aif")
 ;; (defparameter *soundfile* "/home/andersvi/lyd/andersvi/Friday_10.wav")
 ;; (defparameter *soundfile* "C:/Users/Bresson/Desktop/SOUNDFILES/guitar-lick.aif")
 
-
-;; (setq reader (juce::makeAudioFileReader *soundfile*))
-;; (juce::startAudioSource +test-juce-player+ reader)
-;; (juce::stopAudioSource +test-juce-player+ reader)
-;; (juce::setAudioSourceGain reader 0.1)
-;; (juce::setAudioSourcePos reader 0)
-;; (juce::startAudioSource +test-juce-player+ reader)
-;; (juce::freeAudioFileReader reader)
+; (play 0 *soundfile*)
